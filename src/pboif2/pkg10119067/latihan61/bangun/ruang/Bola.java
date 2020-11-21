a75a180e18264c649b5137b2e805a5f328b0cc24
User <User@DESKTOP-6N6EHH6>
2020-11-21 23:05:41 +0700

    PBOIF2-10119067

---
+++ src/pboif2/pkg10119067/latihan61/bangun/ruang/Bola.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119067.latihan61.bangun.ruang;
+
+/**
+ *
+ * @author Danhago
+ * NAMA     : Andreas Danhago P.I
+ * KELAS    : IF2
+ * NIM      : 10119067
+ * Deskripsi: Bangun Ruang
+ */
+public class Bola extends BangunRuang{
+
+    public Bola(int r) {
+        super(r);
+    }
+    
+    public double hitungVolume(){
+        return (4 * 3.14 * (getR() * getR() * getR()))/3;
+    }
+
+    public void tampilHasil() {
+        System.out.println("Volume Bola : " + hitungVolume() + " cm3");
+    }
+
+    private double getR() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+}
